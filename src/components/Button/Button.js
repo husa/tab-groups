5187e742c0b3450866ead102c1befe0e61977ce2
Yuriy Husnay <husnaj23@gmail.com>
2017-10-09 22:07:12 +0300

    add colors, button styles

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,13 +1,26 @@
 import './Button.scss';
 
 import React from 'react';
+import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
 
-const Button = ({type, children, ...rest}) => (
-  <button className={classNames('button', `button--${type}`)} {...rest}>
+const Button = ({type, children, outline, ...rest}) => (
+  <button className={classNames('button', `button--${type}`, {
+    'button--outline': outline
+  })} {...rest}>
     {children}
   </button>
 );
+
+Button.propTypes = {
+  type: PropTypes.oneOf(['primary', 'secondary', 'default']),
+  outline: PropTypes.bool
+};
+
+Button.defaultProps = {
+  type: 'default',
+  outline: false
+};
 
 export default Button;
