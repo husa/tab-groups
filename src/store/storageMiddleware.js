23483bb8ef1d52988c8d99902680e2e131204e41
Yuriy Husnay <husnaj23@gmail.com>
2017-10-26 13:55:27 +0300

    sync state with storage through middleware

--- src/store/storageMiddleware.js
+++ src/store/storageMiddleware.js
@@ -1,23 +1,51 @@
 import groupService from '../services/groups';
 
 import {
-  CREATE_NEW_GROUP
-} from '../actions/groups';
-import {
-  selectGroupIds
+  selectGroupIds,
+  selectGroup
 } from '../selectors';
 
+import {
+  CREATE_NEW_GROUP,
+  ADD_TAB_TO_GROUP,
+  DELETE_TAB
+} from '../actions/groups';
 
-const actions = [
+
+const syncGroupIdsActions = [
   CREATE_NEW_GROUP
 ];
 
+const syncGroupIds = state => {
+  const groupIds = selectGroupIds(state);
+  return groupService.saveGroupIds(groupIds);
+};
+
+const syncGroup = (state, id) => {
+  const group = selectGroup(state, id);
+  return groupService.saveGroup(group);
+};
+
 const storageMiddleware = store => next => action => {
   const result = next(action);
-  if (!actions.includes(action.type)) return result;
+
   const state = store.getState();
-  const groupIds = selectGroupIds(state);
-  groupService.saveGroupIds(groupIds);
+
+  // create new group - save new group
+  if (action.type === CREATE_NEW_GROUP) {
+    const id = action.payload.group.id;
+    syncGroup(state, id);
+  }
+
+  if (action.type === ADD_TAB_TO_GROUP || action.type === DELETE_TAB) {
+    const id = action.payload.groupId;
+    syncGroup(state, id);
+  }
+
+  if (syncGroupIdsActions.includes(action.type)) {
+    syncGroupIds(state);
+  }
+
   return result;
 };
 
