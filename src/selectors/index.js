0e41572d67b419a7da21ef7c98bab8fff9273bfe
Yuriy Husnay <husnaj23@gmail.com>
2017-10-26 13:54:34 +0300

    add selectGroup by id selector

--- src/selectors/index.js
+++ src/selectors/index.js
@@ -1,2 +1,3 @@
 export const selectGroupIds = state => state.groups.ids;
 export const selectGroups = state => selectGroupIds(state).map(id => state.groups[id]);
+export const selectGroup = (state, groupId) => selectGroups(state).find(({id}) => id === groupId);
