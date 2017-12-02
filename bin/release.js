580bc9f77a74d66ab27e6849488b9d3e9ce8e873
Yuriy Husnay <husnaj23@gmail.com>
2017-12-02 14:20:55 +0200

    update release procedure
    add support to major\minor\patch release

--- bin/release.js
+++ bin/release.js
@@ -1,24 +1,42 @@
 const path = require('path');
 const fs = require('fs');
 const git = require('simple-git/promise')();
+const semver = require('semver');
 
 const manifestPath = path.resolve('./src/manifest.json');
 
 const versionRegEx = /^(\d+\.){2}\d+$/; // only numeric versions
-const version = process.argv.pop();
+const versionTypes = ['major', 'minor', 'patch'];
+const versionArg = process.argv.pop();
 
 const invalidVersionMessage = `
+🤦 🤦 🤦
 Version parameter is either missing or invalid
 
 Example usage:
   node bin/release.js 1.2.3
+  node bin/release.js minor
 `;
 
 const unknownErrorMessage = `
-😱  SHIT!!! 💩💩💩
+😱  SHIT!!! 💩 💩 💩
 
 Something went wrong
 `;
+
+const invalidManifestVersionMessage = `
+
+😱 😱 😱 INVALID VERSION IN src/manifest.json
+
+`;
+
+const getManifestVersion = () => new Promise((resolve, reject) => {
+  fs.readFile(manifestPath, 'utf-8', (err, json) => {
+    if (err) return reject(err);
+    const manifest = JSON.parse(json);
+    return resolve(manifest.version);
+  });
+});
 
 const updateManifest = version =>
   new Promise((resolve, reject) => {
@@ -38,12 +56,24 @@ const updateManifest = version =>
   });
 
 (async () => {
-  if (!versionRegEx.test(version)) {
+  if (!versionRegEx.test(versionArg) && !versionTypes.includes(versionArg)) {
     console.log(invalidVersionMessage);
     process.exit(1);
   }
 
   try {
+    let version;
+    if (versionTypes.includes(versionArg)) {
+      const manifestVersion = await getManifestVersion();
+      if (!semver.valid(manifestVersion)) {
+        console.log(invalidManifestVersionMessage)
+      }
+      version = semver.inc(manifestVersion, versionArg);
+    } else {
+      version = versionArg;
+    }
+
+    console.log(`Will update to ${version}`);
     // update version in ./src/manifest.json
     await updateManifest(version);
     // git add ./src/manifest.json
