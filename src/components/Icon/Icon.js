0af5fe5fc6a8479df9b6c84a762ade72ed4a73ee
Yuriy Husnay <husnaj23@gmail.com>
2017-12-08 18:24:04 +0200

    add support for custom classNames

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -3,13 +3,15 @@
 import './Icon.scss';
 
 import React from 'react';
+import classNames from 'classnames';
 
 type Props = {
-  name: string
+  name: string,
+  className?: string
 };
 
-const Icon = ({name}: Props) => (
-  <svg className="icon" viewBox="0 0 24 24" widht="24" height="24">
+const Icon = ({name, className}: Props) => (
+  <svg className={classNames(className, 'icon')} viewBox="0 0 24 24" widht="24" height="24">
     <use xlinkHref={`#${name}`} />
   </svg>
 );
